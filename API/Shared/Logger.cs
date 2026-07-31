71fdee946d998bb76cc29c75ff6a3865b75ffe3a
Retype15 <reynierramos280@gmail.com>
2026-07-30 23:32:03 -0400

    feat: Added Verbosity level to Logger.

--- API/Shared/Logger.cs
+++ API/Shared/Logger.cs
@@ -12,45 +12,58 @@ using Microsoft.Xna.Framework;
 
 namespace SOS
 {
+    internal enum LogLevel
+    {
+        Deactivate,
+        Critical,
+        Standard,
+        Extended,
+        Verbose,
+    }
+
     internal static class Logger
     {
         internal static ILoggerService? LoggerService = null;
+        internal static LogLevel ActualLogLevel = LogLevel.Standard;
 
         [Conditional("DEBUG")]
-        public static void LogDebug(string message, Color? color = null, ILoggerService? logger = null) => Log(message, color, logger);
+        public static void LogDebug(string? message, Color? color = null, ILoggerService? logger = null, LogLevel level = LogLevel.Standard) => Log(message, color, logger, level);
 
         [Conditional("DEBUG")]
-        public static void LogDebugError(string message, ILoggerService? logger = null) => LogError(message, logger);
+        public static void LogDebugError(string? message, ILoggerService? logger = null, LogLevel level = LogLevel.Critical) => LogError(message, logger, level);
 
         [Conditional("DEBUG")]
-        public static void LogDebugWarning(string message, ILoggerService? logger = null) => LogWarning(message, logger);
+        public static void LogDebugWarning(string? message, ILoggerService? logger = null, LogLevel level = LogLevel.Standard) => LogWarning(message, logger, level);
 
         [Conditional("RELEASE")]
-        public static void LogRelease(string message, Color? color = null, ILoggerService? logger = null) => Log(message, color, logger);
+        public static void LogRelease(string? message, Color? color = null, ILoggerService? logger = null, LogLevel level = LogLevel.Standard) => Log(message, color, logger, level);
 
         [Conditional("RELEASE")]
-        public static void LogReleaseError(string message, ILoggerService? logger = null) => LogError(message, logger);
+        public static void LogReleaseError(string? message, ILoggerService? logger = null, LogLevel level = LogLevel.Critical) => LogError(message, logger, level);
 
         [Conditional("RELEASE")]
-        public static void LogReleaseWarning(string message, ILoggerService? logger = null) => LogWarning(message, logger);
+        public static void LogReleaseWarning(string? message, ILoggerService? logger = null, LogLevel level = LogLevel.Standard) => LogWarning(message, logger, level);
 
-        public static void Log(string message, Color? color = null, ILoggerService? logger = null)
+        public static void Log(string? message, Color? color = null, ILoggerService? logger = null, LogLevel level = LogLevel.Standard)
         {
+            if (level > ActualLogLevel || string.IsNullOrEmpty(message)) return;
             color ??= Color.SkyBlue;
             if (logger != null) logger.Log(message, color);
             else if (LoggerService != null) LoggerService.Log(message, color);
             else LuaCsLogger.Log(message, color);
         }
 
-        public static void LogError(string message, ILoggerService? logger = null)
+        public static void LogError(string? message, ILoggerService? logger = null, LogLevel level = LogLevel.Critical)
         {
+            if (level > ActualLogLevel || string.IsNullOrEmpty(message)) return;
             if (logger != null) logger.LogError(message);
             else if (LoggerService != null) LoggerService.LogError(message);
             else LuaCsLogger.LogError(message);
         }
 
-        public static void LogWarning(string message, ILoggerService? logger = null)
+        public static void LogWarning(string? message, ILoggerService? logger = null, LogLevel level = LogLevel.Standard)
         {
+            if (level > ActualLogLevel || string.IsNullOrEmpty(message)) return;
             if (logger != null) logger.LogWarning(message);
             else if (LoggerService != null) LoggerService.LogWarning(message);
             else LuaCsLogger.Log(message, Color.Yellow);
